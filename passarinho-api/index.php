635abf861735c85b8acea17b5b03d4904d35bf29
Jefferson Chaves <jeffersonchaves@users.noreply.github.com>
2022-11-17 07:27:03 -0300

    Add files via upload

--- passarinho-api/index.php
+++ passarinho-api/index.php
@@ -1,37 +1,63 @@
 <?php
 
-require_once "vendor/autoload.php";
+require_once __DIR__ .  "/vendor/autoload.php";
 
 use Slim\App;
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Slim\Container;
 
-$app = new App();
+$configuration = [
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
+];
 
-$app->get('/', function(Request $request, Response $response){
-    $response->write("Olá Slim (index)");
+$c = new Container($configuration);
+$app = new App($c);
+
+$app->get('/', function(Request $request, Response $response, array $parameters){
+    return $response->write("<h1>olá slim</h1>");
 });
 
-$app->get('/recados[/{id}]', function(Request $request, Response $response, $parametros){
-
-    $pathParameters = $request->getParams();
-    $queryParameters = $request->getQueryParams();
-
-    $outrosParametros = $parametros;
-
-    $cabecalhos = $request->getHeaders();
-
-    $dados = [
-        "mensagem" => "Olá Slim (recados)",
-        "parametros" => $queryParameters,
-        "opcoes" => $pathParameters,
-        "outros" => $outrosParametros,
-        "headers" => $cabecalhos
-    ];
+$app->get('/recados', function(Request $request, $response, $parameters){
     
-    $response->write(json_encode($dados));
+    $limite = $request->getQueryParam('limite') ?? 10;
+    $pagina = $request->getQueryParam('pagina') ?? 1;
 
-    return $response;   
+    $pagina =  ($pagina - 1) * $limite;
+
+    $host = "localhost";
+    $user = "root";
+    $password = "password#22"; //bancodedados
+
+    $connection = new PDO("mysql:host=$host;dbname=passarinho_api_v1", $user, $password);
+
+    $query = $connection->query("SELECT * FROM tb_recados LIMIT $limite OFFSET $pagina");
+
+    $lista_recados = $query->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+
+    $recados = [
+        "pagina" => $pagina,
+        "limite" => $limite,
+        "recados " =>  $lista_recados
+    ];
+
+    return $response->withJson($recados);
+});
+
+$app->get("/recados/{id}", function(Request $request, Response $response, array $parameters){
+
+    $id = $parameters['id'];
+
+    $recados = [
+        "id_recado" => $id
+    ];
+
+    return $response->withJson($recados);
 
 });
 
